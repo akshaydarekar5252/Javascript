e547f725e6bb55f142ecb2821c6e8f8a1976c625
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-02-18 23:20:45 +0530

    javascript part 9

---
+++ javascript part 9/script.js
@@ -0,0 +1,9 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+// for (let i=0; i< smallImages.length; i++){
+//     console.log(smallImages[i]);
+// }
+
+console.dir(document.querySelector("h1"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+console.dir(document.querySelectorAll("div a"));
