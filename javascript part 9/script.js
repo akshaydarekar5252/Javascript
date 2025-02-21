755ce15faabac659d501b155ac5efa5adef9459f
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-02-21 08:41:53 +0530

    Javascript part 9

--- javascript part 9/script.js
+++ javascript part 9/script.js
@@ -3,7 +3,35 @@
 //     console.log(smallImages[i]);
 // }
 
-console.dir(document.querySelector("h1"));
-console.dir(document.querySelector("#description"));
-console.dir(document.querySelector(".oldImg"));
-console.dir(document.querySelectorAll("div a"));
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelectorAll("div a"));
+
+
+// let links = document.querySelectorAll(".box a");
+
+// // for( let i=0; i < links.length; i++ ) {
+// //     links[i].style.color = "yellow";
+// // }
+
+// //or 
+
+// for(link of links){
+//     link.style.color = "red"; //inline style 
+// }
+
+
+
+
+//practice question 
+
+let para = document.createElement("p");
+para.innerText="Hey I'm red!";
+document.querySelector("body").append(para);
+para.classList.add("red");
+
+let h3 = document.createElement("h3");
+h3.innerText="Hey I'm blue h3!";
+document.querySelector("body").append(h3);
+h3.classList.add("blue");
